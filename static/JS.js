b1600c622efc4ecd4ffa2ba22ab254f7e8f3cc97
nexustcc4-hub <nexus.tcc4@gmail.com>
2025-08-25 19:58:22 +0000

    arrumando js

--- static/JS.js
+++ static/JS.js
@@ -99,22 +99,33 @@ function continuarGoogle() {
         }
 
 
-function handleCredentialResponse(response) {
-    console.log("JWT ID Token: ", response.credential);
+function decodeJWT(token) {
 
-    // Enviar token para o backend
-    fetch("http://localhost:3000/auth/google", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ token: response.credential })
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log("Resposta do backend:", data);
-        alert("Logado como " + data.user.name);
-    })
-    .catch(err => console.error("Erro:", err));
-}
+        let base64Url = token.split(".")[1];
+        let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        let jsonPayload = decodeURIComponent(
+          atob(base64)
+            .split("")
+            .map(function (c) {
+              return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join("")
+        );
+        return JSON.parse(jsonPayload);
+      }
 
+      function handleCredentialResponse(response) {
+
+        console.log("Encoded JWT ID token: " + response.credential);
+
+        const responsePayload = decodeJWT(response.credential);
+
+        console.log("Decoded JWT ID token fields:");
+        console.log("  Full Name: " + responsePayload.name);
+        console.log("  Given Name: " + responsePayload.given_name);
+        console.log("  Family Name: " + responsePayload.family_name);
+        console.log("  Unique ID: " + responsePayload.sub);
+        console.log("  Profile image URL: " + responsePayload.picture);
+        console.log("  Email: " + responsePayload.email);
+        console.log("  VerifiedEmail: " + responsePayload.email_verified);
+      }
